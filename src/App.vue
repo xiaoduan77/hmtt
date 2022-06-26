50cb95a62916995b2ee91dbd9716addb67e49a20
xiaoduan77 <1563534677@qq.com>
2022-06-26 10:11:18 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>123
+    爱他的心git push -u origin master --force
+  </div>
+</template>
+
+<script>
+export default {
+  created() { },
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  filters: {},
+  components: {}
+}
+</script>
+
+<style scoped lang='less'>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '黑马头条'
+        return args
+      })
+  }
+
+})
